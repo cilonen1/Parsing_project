a9096333fc38755b9b2993a052c5b7bf05d285fb
cilonen1 <cilonen1@gmail.com>
2022-04-06 23:53:29 +0300

    add args to range seasons

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ parser.add_argument('league', type=str)
 parser.add_argument('start_year', type=int)
 parser.add_argument('end_year', type=int)
 args = parser.parse_args()
-for year in range(start_year, end_year+1):
+for year in range(args.start_year, args.end_year + 1):
     odds_df = collect_data(args.league, year)
     country = args.league.split('/')[0]
     odds_df.to_excel(f'{country}_{year}.xlsx')
