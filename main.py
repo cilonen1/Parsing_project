03e01bdbc636e5881f7641ceb5c8b7135c313139
cilonen1 <cilonen1@gmail.com>
2022-04-06 23:45:53 +0300

    add seasons range to parse

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from scraping import collect_data
 
 parser = argparse.ArgumentParser(description='Collect league and year')
 parser.add_argument('league', type=str)
-parser.add_argument('year', type=str)
+parser.add_argument('start_year', type=int)
+parser.add_argument('end_year', type=int)
 args = parser.parse_args()
-odds_df = collect_data(args.league, args.year)
-country = args.league.split('/')[0]
-odds_df.to_excel(f'{country}_{args.year}.xlsx')
+for year in range(start_year, end_year+1):
+    odds_df = collect_data(args.league, year)
+    country = args.league.split('/')[0]
+    odds_df.to_excel(f'{country}_{year}.xlsx')
